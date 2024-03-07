22ff38a6094753ddab419a82b95fd6efc9b198a4
Kris Ly <chily2808@gmail.com>
2024-03-06 21:48:38 -0600

    Adding back end

--- src/server.js
+++ src/server.js
@@ -1,27 +1,46 @@
+/* eslint-disable no-console */
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import {env} from '~/config/environment'
+import exitHook from 'async-exit-hook'
+import {CONNECT_DB, CLOSE_DB} from '~/config/mongodb'
 
-const app = express()
+const START_SERVER = () =>{
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  app.get('/', async(req, res) => {
+    // console.log(process.env)
+    res.end('<h1>Hello World Kris Ly!</h1><hr>')
+  })
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`3. Hello ${env.AUTHOR}, I am running at ${env.APP_HOST}:${env.APP_PORT}/`)
+  })
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  // eslint-disable-next-line no-console
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' }],
-    ['id-5', 'id-3', 'id-2', 'id-4', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World Kris Ly!</h1><hr>')
-})
+  //Close MongoDB connection, cleanning up
+  exitHook(() => {
+    console.log('4. Disconnecting..')
+    CLOSE_DB()
+    console.log('5. Disconnected.')
+  })
+}
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Kris Ly, I am running at ${ hostname }:${ port }/`)
-})
+//Invoked Immediately/ IIFE
+(async () => {
+  try {
+    console.log('1. Connecting to MongoDB Cloud Atlas!')
+    await CONNECT_DB()
+    console.log('2. Connected to MongoDB Cloud Atlas!')
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
+
+//Different way to invoke the database
+// CONNECT_DB()
+//   .then(() => console.log('Connected to MongoDB Cloud Atlas!'))
+//   .then(() => START_SERVER())
+//   .catch(error => {
+//     console.error(error)
+//     process.exit(0)
+//   })
