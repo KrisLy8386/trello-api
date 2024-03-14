dd3a9f847d1f3f275454b7ff8cf808bf2a769550
Kris Ly <chily2808@gmail.com>
2024-03-13 22:32:49 -0500

    Add mongo aggregate

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -2,6 +2,7 @@
 import * as Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
 import ApiError from '~/utils/ApiError'
+import { BOARD_TYPES } from '~/utils/constants'
 
 const createNew = async (req, res, next) => {
 
@@ -16,7 +17,8 @@ const createNew = async (req, res, next) => {
     }),
 
     //default message
-    description: Joi.string().required().min(3).max(256).trim().strict()
+    description: Joi.string().required().min(3).max(256).trim().strict(),
+    type: Joi.string().valid(BOARD_TYPES.PUBLIC, BOARD_TYPES.PRIVATE).required()
   })
 
   try {
