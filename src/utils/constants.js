d894feae61cfc41519f1950469ecec5ffbf021e5
Kris Ly <chily2808@gmail.com>
2024-03-20 22:44:54 -0500

    add deploy link

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173' // Không cần localhost nữa vì ở file config/cors đã luôn luôn cho phép môi trường dev (env.BUILD_MODE === 'dev')
 
   // Lưu ý: Đây là domain ví dụ sau khi Deploy Production (xem video 75 và video 76 để hiểu rõ kiến thức phần này, còn hiện tại mình đã xóa domain này rồi, đừng cố truy cập làm gì =))
-  'https://trello-api-sw51.onrender.com'
+  'https://trello-web-mu.vercel.app/'
 ]
 
 export const BOARD_TYPES = {
